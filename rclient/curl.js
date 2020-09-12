d139991d894065d27eab08b5913321846ce279d1
Dan Connolly <dckc@madmode.com>
2020-09-11 22:04:58 -0500

    nodeFetch: implement fetch api based on node http

--- rclient/curl.js
+++ rclient/curl.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 /**
  * @param {string} url
  * @param {{ http: any }} powers
@@ -16,4 +18,41 @@ export function curl(url, { http }) {
     req.end();
     req.on('error', reject);
   });
+}
+
+/**
+ * @returns { typeof fetch }
+ */
+export function nodeFetch({ http }) {
+  /** @type { typeof fetch } */
+  function fetch(url, options = {}) {
+    return new Promise((resolve, reject) => {
+      const { method = 'GET', body } = options;
+      if (typeof url !== 'string') { throw new Error('not implemented'); }
+      const req = http.request(url, { method }, res => {
+        res.setEncoding('utf8');
+        let content = ''
+        res.on('data', data => { content += data; });
+
+        res.on('end', () => {
+          /** @type any */
+          const response = {
+            ok: res.statusCode && res.statusCode >= 200 && res.statusCode < 300,
+            status: res.statusCode,
+            statusText: res.statusMessage,
+            text: () => content,
+            json: () => JSON.parse(content),
+          };
+          resolve(response);
+        });
+      });
+      if (body !== undefined) {
+        req.write(body);
+      }
+      req.on('error', reject);
+      req.end();
+    });
+  }
+
+  return fetch;
 }
