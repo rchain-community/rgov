7e53ec6b2c09131bcc7db95991b8f3e08f7119f1
Dan Connolly <dckc@madmode.com>
2020-10-23 16:10:35 -0500

    provide myGovRevAddr from eth provider rather than from deployerId
     - ts-check for actions: ActionSpec, FieldSpec
     - add walletRevAddr field type
     - factor out optionSelected

--- src/actions.js
+++ src/actions.js
@@ -1,33 +1,30 @@
+// @ts-check
+
+/**
+ * @typedef {{ template: string, fields?: Record<string, FieldSpec> }} ActionSpec
+ * @typedef {{ type: 'string' | 'uri' | 'walletRevAddr', value?: string }} FieldSpec
+ * @type {Record<string, ActionSpec>}
+ */
 export const actions = {
   helloWorld: {
     template: `new world in { world!("Hello!") }`,
   },
   checkRegistration: {
     fields: {
+      myGovRevAddr: { type: 'walletRevAddr' },
       votersUri: {
         value: 'rho:id:admzpibb3gxxp18idri7h6eneg4io6myfmcmjhufc6asy73bgrojop',
         type: 'uri',
       },
     },
     template: `
-    new return(\`rho:rchain:deployId\`),
+    new return,
       lookup(\`rho:registry:lookup\`),
-      deployerId(\`rho:rchain:deployerId\`),
-      DeployerIdOps(\`rho:rchain:deployerId:ops\`),
-      deployerPubKeyBytesCh,
-      revAddrCh,
-      RevAddress(\`rho:rev:address\`),
       ch in
     {
-      DeployerIdOps!("pubKeyBytes", *deployerId, *deployerPubKeyBytesCh) |
-      for (@deployerPubKeyBytes <- deployerPubKeyBytesCh) {
-        RevAddress!("fromPublicKey", deployerPubKeyBytes, *revAddrCh) |
-        for (@deployerRevAddress <- revAddrCh) {
-          lookup!(votersUri, *ch) |
-          for (@addrSet <- ch) {
-            return!(addrSet.contains(deployerRevAddress))
-          }
-        }
+      lookup!(votersUri, *ch) |
+      for (@addrSet <- ch) {
+        return!(addrSet.contains(myGovRevAddr))
       }
     }`,
   },
--- src/participate.js
+++ src/participate.js
@@ -2,7 +2,12 @@
 import { actions } from './actions.js';
 import 'https://unpkg.com/htm@3.0.4/dist/htm.js';
 import 'https://unpkg.com/mithril@2.0.4/mithril.js'; // m
-
+import {
+  getEthProvider,
+  MetaMaskAccount,
+} from '../node_modules/rchain-api/src/ethProvider.js';
+// TODO: switch to webpack for bare imports such as js-sha3
+// import { getAddrFromEth } from '../node_modules/rchain-api/src/rev-address.js';
 const { freeze, keys, entries } = Object;
 
 /**
@@ -25,9 +30,17 @@ const html = htm.bind(m);
 
 const state = {
   action: 'helloWorld',
+  /** @type {Record<string, import('./actions').FieldSpec>} */
   fields: {},
   term: '',
 };
+
+const optionSelected = (/** @type { boolean } */ bool) =>
+  bool ? { selected: 'selected' } : {};
+
+const metaMaskP = getEthProvider({ window }).then((ethereum) =>
+  MetaMaskAccount(ethereum),
+);
 
 m.mount(
   $('#actionControl'),
@@ -35,19 +48,40 @@ m.mount(
     view() {
       const options = keys(actions).map(
         (id) =>
-          html`<option
-            value=${id}
-            ...${id === state.action ? { selected: 'selected' } : {}}
-          >
+          html`<option value=${id} ...${optionSelected(id === state.action)}>
             ${id}
           </option>`,
       );
 
       const fields = entries(state.fields).map(
-        ([name, { value }]) =>
+        ([name, { value, type }]) =>
           html`<br /><label
-              >${name}: <input name=${name} value=${value}
-            /></label>`,
+              >${name}:
+              <input
+                name=${name}
+                value=${value}
+                onchange=${(/** @type {Event} */ event) => {
+                  state.fields[name].value = event.target.value;
+                }}
+            /></label>
+            ${type === 'walletRevAddr'
+              ? html`<button
+                  onclick=${(/** @type {Event} */ event) => {
+                    event.preventDefault();
+                    metaMaskP.then((mm) =>
+                      mm.ethereumAddress().then((ethAddr) => {
+                        console.log('@@@', { ethAddr });
+                        // TODO:
+                        // const revAddr = getAddrFromEth(ethAddr);
+                        state.fields[name].value = ethAddr;
+                        m.redraw();
+                      }),
+                    );
+                  }}
+                >
+                  Connect Wallet
+                </button>`
+              : []}`,
       );
 
       if (keys(state.fields).length === 0) {
@@ -69,7 +103,6 @@ m.mount(
           <select
             name="action"
             onchange=${(/** @type {Event} */ event) => {
-              console.log(event);
               state.action = event.target.value;
               state.fields = actions[state.action].fields || {};
             }}
