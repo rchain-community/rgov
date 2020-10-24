bb26e81d4106cb9cba7b0f8e2f1bd3c3b6539b7f
Dan Connolly <dckc@madmode.com>
2020-10-23 22:23:44 -0500

    webpack config
      - tweak tsc config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  entry: './src/participate.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  node: {
+    // Buffer: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/participate.html',
+      filename: 'index.html',
+    }),
+  ],
+};
