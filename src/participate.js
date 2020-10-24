ea9d981b89cf2c9f63b9882eb528e39487e1a6e0
Dan Connolly <dckc@madmode.com>
2020-10-23 22:24:07 -0500

    participate.js: import default for htm, mithril
     - refactor access to ambient authority

--- src/participate.js
+++ src/participate.js
@@ -1,14 +1,9 @@
 // @ts-check
+import htm from 'htm';
+import m from 'mithril';
+import { getEthProvider, MetaMaskAccount, getAddrFromEth } from 'rchain-api';
 import { actions } from './actions.js';
-import 'https://unpkg.com/htm@3.0.4/dist/htm.js';
-import 'https://unpkg.com/mithril@2.0.4/mithril.js'; // m
-import {
-  getEthProvider,
-  MetaMaskAccount,
-} from '../node_modules/rchain-api/src/ethProvider.js';
-// TODO: switch to webpack for bare imports such as js-sha3
-// import { getAddrFromEth } from '../node_modules/rchain-api/src/rev-address.js';
-const { freeze, keys, entries } = Object;
+const { freeze, keys, values, entries } = Object;
 
 /**
  * @param {T?} x
@@ -20,100 +15,152 @@ function the(x) {
   return x;
 }
 
-/** @type {(selector: string) => HTMLElement} */
-const $ = (selector) => the(document.querySelector(selector));
-
-console.log('hi from participate.js');
-console.log({ m, htm });
-
-const html = htm.bind(m);
-
-const state = {
-  action: 'helloWorld',
-  /** @type {Record<string, import('./actions').FieldSpec>} */
-  fields: {},
-  term: '',
-};
-
 const optionSelected = (/** @type { boolean } */ bool) =>
   bool ? { selected: 'selected' } : {};
 
-const metaMaskP = getEthProvider({ window }).then((ethereum) =>
-  MetaMaskAccount(ethereum),
-);
+document.addEventListener('DOMContentLoaded', () => {
+  /** @type {(selector: string) => HTMLElement} */
+  const $ = (selector) => the(document.querySelector(selector));
 
-m.mount(
-  $('#actionControl'),
-  freeze({
-    view() {
-      const options = keys(actions).map(
-        (id) =>
-          html`<option value=${id} ...${optionSelected(id === state.action)}>
-            ${id}
-          </option>`,
+  const html = htm.bind(m);
+
+  buildUI({
+    html,
+    getEthProvider: () => getEthProvider({ window }),
+    mount: (selector, control) => m.mount($(selector), control),
+    redraw: () => m.redraw(),
+  });
+});
+
+/**
+ * @param { HTMLBuilder & EthSignAccess & MithrilMount } io
+ * @typedef {import('./actions').FieldSpec} FieldSpec
+ * @typedef {{
+ *   html: any, // TODO: htm(m) type
+ * }} HTMLBuilder
+ *
+ * @typedef {{
+ *   mount: (selector: string, component: import('mithril').Component) => void,
+ *   redraw: () => void,
+ * }} MithrilMount
+ * @typedef {{
+ *   getEthProvider: () => Promise<MetaMaskProvider>
+ * }} EthSignAccess
+ * @typedef { import('rchain-api/src/ethProvider').MetaMaskProvider } MetaMaskProvider
+ */
+function buildUI({ html, getEthProvider, mount }) {
+  const state = {
+    action: 'helloWorld',
+    /** @type {Record<string, FieldSpec>} */
+    fields: {},
+    term: '',
+  };
+
+  mount('#actionControl', actionControl(state, { html, getEthProvider }));
+}
+
+/**
+ * @param { Event } event
+ * @returns { string }
+ */
+function eventValue(event) {
+  const ctrl = event.target;
+  if (
+    !(ctrl instanceof HTMLInputElement) &&
+    !(ctrl instanceof HTMLSelectElement)
+  )
+    throw TypeError(String(ctrl));
+  return ctrl.value;
+}
+
+/**
+ * @param {{
+ *   action: string,
+ *   fields: Record<string, FieldSpec>,
+ *   term: string,
+ * }} state
+ * @param {HTMLBuilder & EthSignAccess} io
+ */
+function actionControl(state, { html, getEthProvider }) {
+  const options = (/** @type {string[]}*/ ids) =>
+    ids.map(
+      (id) =>
+        html`<option value=${id} ...${optionSelected(id === state.action)}>
+          ${id}
+        </option>`,
+    );
+
+  function selectAction(/** @type { string } */ action) {
+    state.action = action;
+    state.fields = actions[state.action].fields || {};
+    if (keys(state.fields).length === 0) {
+      state.term = actions[state.action].template;
+    } else {
+      const exprs = values(state.fields).map(({ value, type }) =>
+        type === 'uri' ? `\`${value}\`` : JSON.stringify(value),
       );
-
-      const fields = entries(state.fields).map(
-        ([name, { value, type }]) =>
-          html`<br /><label
-              >${name}:
-              <input
-                name=${name}
-                value=${value}
-                onchange=${(/** @type {Event} */ event) => {
-                  state.fields[name].value = event.target.value;
-                }}
-            /></label>
-            ${type === 'walletRevAddr'
-              ? html`<button
-                  onclick=${(/** @type {Event} */ event) => {
-                    event.preventDefault();
-                    metaMaskP.then((mm) =>
-                      mm.ethereumAddress().then((ethAddr) => {
-                        console.log('@@@', { ethAddr });
-                        // TODO:
-                        // const revAddr = getAddrFromEth(ethAddr);
-                        state.fields[name].value = ethAddr;
-                        m.redraw();
-                      }),
-                    );
-                  }}
-                >
-                  Connect Wallet
-                </button>`
-              : []}`,
-      );
-
-      if (keys(state.fields).length === 0) {
-        state.term = actions[state.action].template;
-      } else {
-        state.term = `match (${entries(state.fields)
-          .map(([name, { value, type }]) =>
-            type === 'uri' ? `\`${value}\`` : JSON.stringify(value),
-          )
-          .join(', ')}) {
-            (${keys(state.fields).join(', ')}) => ${
-          actions[state.action].template
-        }
-          }`;
+      state.term = `match (${exprs.join(', ')}) {
+          (${keys(state.fields).join(', ')}) => ${
+        actions[state.action].template
       }
+  }`;
+    }
+  }
+
+  const metaMaskP = getEthProvider().then((ethereum) =>
+    MetaMaskAccount(ethereum),
+  );
+
+  const fields = (/** @type {Record<string, FieldSpec>} */ fields) =>
+    entries(fields).map(
+      ([name, { value, type }]) =>
+        html`<br /><label
+            >${name}:
+            <input
+              name=${name}
+              value=${value}
+              onchange=${(/** @type {Event} */ event) => {
+                state.fields[name].value = eventValue(event);
+              }}
+          /></label>
+          ${type === 'walletRevAddr'
+            ? html`<button
+                onclick=${(/** @type {Event} */ event) => {
+                  event.preventDefault();
+                  metaMaskP.then((mm) =>
+                    mm.ethereumAddress().then((ethAddr) => {
+                      console.log('@@@', { ethAddr });
+                      const revAddr = getAddrFromEth(ethAddr);
+                      if (!revAddr) throw new Error('bad ethAddr???');
+                      state.fields[name].value = revAddr;
+                      selectAction(state.action); // rebuild term. hm.
+                      m.redraw();
+                    }),
+                  );
+                }}
+              >
+                Connect Wallet
+              </button>`
+            : []}`,
+    );
+
+  return freeze({
+    view() {
       return html`
         <label
           >Action:
           <select
             name="action"
-            onchange=${(/** @type {Event} */ event) => {
-              state.action = event.target.value;
-              state.fields = actions[state.action].fields || {};
-            }}
+            onchange=${(/** @type {Event} */ event) =>
+              selectAction(eventValue(event))}
           >
-            ${options}
+            ${options(keys(actions))}
           </select>
         </label>
-        ${fields}
+        ${fields(state.fields)}
         <br />
         <textarea cols="80" rows="16">${state.term}</textarea>
       `;
     },
-  }),
-);
+  });
+}
