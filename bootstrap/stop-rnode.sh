c4fae17482e0ea4b86ce72c3f5ec1965c4315be1
Bill Keuntje <bill.keuntje@gmail.com>
2021-03-28 02:48:40 -0700

    fix stupid grep problem

--- bootstrap/stop-rnode.sh
+++ bootstrap/stop-rnode.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-ALREADY=`ps a |grep -v grep |grep rnode|sed 's/[ \t][ \t]*/ /g'|cut -d' ' -f 2`
+ALREADY=`ps a |grep -v grep |grep -v $0|grep rnode|sed 's/[ \t][ \t]*/ /g'|cut -d' ' -f 2`
 
 [ -n "$ALREADY" ] && echo "
-$0: rnode is currently running
-Use 'kill $ALREADY' to fix.
+$0: rnode is currently running. Use 'kill $ALREADY' to fix.
 " && while read -p "Execute 'kill $ALREADY' [y]? " response;do
 	if [ "$response" == "y" ] || [ -z "$response" ];then
       set -x
