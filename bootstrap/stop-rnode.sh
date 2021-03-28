2c566b6ec442d6a2b222cf44d3b8b01152acfa05
Bill Keuntje <bill.keuntje@gmail.com>
2021-03-27 23:07:39 -0700

    Working master directory lookup with Echo class

---
+++ bootstrap/stop-rnode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ALREADY=`ps a |grep -v grep |grep rnode|sed 's/[ \t][ \t]*/ /g'|cut -d' ' -f 2`
+
+[ -n "$ALREADY" ] && echo "
+$0: rnode is currently running
+Use 'kill $ALREADY' to fix.
+" && while read -p "Execute 'kill $ALREADY' [y]? " response;do
+	if [ "$response" == "y" ] || [ -z "$response" ];then
+      set -x
+		kill $ALREADY
+      set +x
+		break
+	else
+		echo "Aborting $0"
+		exit 1
+	fi
+done
