abb35502654761bc9bd6290eb812154132c0c00c
Tom Moulton <tom@moulton.us>
2021-04-24 11:44:36 -0400

    Make scripts call stop-rnode script instead of had coding. Also aborts the script if use declines to Kill the process

--- bootstrap/create-checkpoint.sh
+++ bootstrap/create-checkpoint.sh
@@ -4,24 +4,10 @@
 
 [ ! -d ~/.rnode ] && echo "$HOME/.rnode does not exist for checkpointing" && exit 2
 
-ALREADY=`ps -fu |grep -v grep |grep " java .*rnode"|sed 's/[ \t][ \t]*/ /g'|cut -d' ' -f 2`
-
-[ -n "$ALREADY" ] && echo "
-$0: rnode is currently running
-Use 'kill $ALREADY' to fix.
-" && while read -p "Execute 'kill $ALREADY' [y]? " response;do
-	if [ "$response" == "y" ] || [ -z "$response" ];then
-      set -x
-		kill $ALREADY
-      set +x
-		break
-	else
-		echo "Aborting $0"
-		exit 3
-	fi
-done
-
 cd `dirname $0`
+
+./stop-rnode
+if [ $? -ne 0 ] ; then exit $?; fi
 
 mkdir -p checkpoint
 
--- bootstrap/restore-checkpoint.sh
+++ bootstrap/restore-checkpoint.sh
@@ -2,23 +2,6 @@
 
 [ -z "$1" ] && echo "Please specify checkpoint name" && exit 1
 
-ALREADY=`ps -fu |grep -v grep |grep " java .*rnode"|sed 's/[ \t][ \t]*/ /g'|cut -d' ' -f 2`
-
-[ -n "$ALREADY" ] && echo "
-$0: rnode is currently running
-Use 'kill $ALREADY' to fix.
-" && while read -p "Execute 'kill $ALREADY' [y]? " response;do
-	if [ "$response" == "y" ] || [ -z "$response" ];then
-      set -x
-		kill $ALREADY
-      set +x
-		break
-	else
-		echo "Aborting $0"
-		exit 1
-	fi
-done
-
 [ -d ~/.rnode ] && echo "$HOME/.rnode exists" && while read -p "Replace [y]? " response;do
    if [ "$response" != 'y' ] && [ -n "$response" ];then
       echo "Aborted" && exit 0
@@ -28,6 +11,9 @@ done
 done
 
 cd `dirname $0`
+
+./stop-rnode
+if [ $? -ne 0 ] ; then exit $?; fi
 
 TARGET=$PWD/checkpoint/$1.tgz
 
