df4e65a301979cffb24daac07fa149989cc24b19
Paul Fabre <paulfabre31@gmail.com>
2021-05-31 22:26:16 +0200

    wip

--- src/ballot/ballot.js
+++ src/ballot/ballot.js
@@ -4,46 +4,78 @@ var tableDiv = document.getElementById('table');
 var tableBodyDiv = document.getElementById('table-body');
 
 let state = {
-  issues: {},
+  issues: [
+    {
+      id: 0,
+      desc: 'Who is president ?',
+      type: 'choices',
+      choices: ['Donald Trump', 'Joe Biden', 'Michael Jordan'],
+    },
+    {
+      id: 1,
+      desc: 'Bla',
+      type: 'choices',
+      choices: ['support', 'abstain', 'oppose'],
+    },
+    {
+      id: 2,
+      desc:
+        'Blabla very long issue to make sure nothing overlapse so does it ? I guess it is okay but who knows what happens wit css sometimes',
+      type: 'choices',
+      choices: ['support', 'abstain', 'oppose'],
+    },
+    {
+      id: 3,
+      desc: 'More cycle lanes in the world',
+      type: 'choices',
+      choices: ['support', 'abstain', 'oppose'],
+    },
+  ],
+  votes: {},
 };
 
-const issues = [
-  { id: 0, desc: 'Bla' },
-  {
-    id: 1,
-    desc:
-      'Blabla very long issue to make sure nothing overlapse so does it ? I guess it is okay but who knows what happens wit css sometimes',
-  },
-  { id: 2, desc: 'More cycle lanes in the world' },
-];
-
+// REDUCERS
 const initializeVotes = () => {
-  issues.forEach((iss) => {
-    state.issues[iss.id] = { id: iss.id, vote: null };
+  let newVotes = {};
+  state.issues.forEach((iss) => {
+    newVotes[iss.id] = { vote: null };
   });
+  state = {
+    ...state,
+    votes: newVotes,
+  };
 };
+
+const vote = (e, issueId, vote) => {
+  console.log(e);
+
+  state = {
+    ...state,
+    votes: {
+      ...state.votes,
+      [issueId]: vote,
+    },
+  };
+  console.log(state);
+};
+
 initializeVotes();
 
-const vote = (e) => {
-  e.preventDefault();
-  const row = e.target.getAttribute('row');
-  const role = e.target.getAttribute('role');
-  state.issues[row].vote = role;
-};
+const getIcon = (issueId, vote1) => {
+  console.log('getIcon');
 
-const getIcon = (row, role) => {
-  if (state.issues[row].vote !== role)
+  if (state.votes[issueId].vote === vote1)
     return m('i', {
-      class: 'envelope outline icon',
-      row: row,
-      role: role,
-      onclick: (e) => vote(e),
+      class: 'envelope icon selected ' + vote,
+      row: issueId,
+      role: vote1,
     });
   else
     return m('i', {
-      class: 'envelope icon selected ' + role,
-      row: row,
-      role: role,
+      class: 'envelope outline icon',
+      row: issueId,
+      role: vote1,
+      onclick: (e) => vote(e, issueId, vote1),
     });
 };
 
@@ -65,26 +97,28 @@ m.mount(document.getElementById('table-header'), tableHeader);
 
 const tableBody = {
   view: () => {
-    return issues.map((issue) => {
-      return m('tr', [
-        m('td', { class: 'id-cell' }, issue.id),
-        m('td', { class: 'issue-description' }, issue.desc),
-        m('td', { class: 'selectable' }, [
-          m('a', { class: 'voting-link', href: '' }, [
-            getIcon(issue.id, 'support'),
-          ]),
-        ]),
-        m('td', { class: 'selectable' }, [
-          m('a', { class: 'voting-link', href: '' }, [
-            getIcon(issue.id, 'abstain'),
-          ]),
-        ]),
-        m('td', { class: 'selectable' }, [
-          m('a', { class: 'voting-link', href: '' }, [
-            getIcon(issue.id, 'oppose'),
-          ]),
-        ]),
-      ]);
+    console.log(state.votes);
+    return state.issues.map((issue) => {
+      if (issue.type === 'choices') {
+        return m('tr', [
+          m('td', { class: 'id-cell' }, issue.id),
+          m('td', { class: 'issue-description' }, issue.desc),
+          issue.choices.map((choice) => {
+            return m('td', { class: 'selectable' }, [
+              m(
+                'a',
+                {
+                  class: 'voting-link',
+                  href: '#',
+                },
+                [getIcon(issue.id, choice)],
+              ),
+            ]);
+          }),
+        ]);
+      } else {
+        console.error('Unsupported issue type');
+      }
     });
   },
 };
