5543cd715768d770c32225a95d2b09ae2bcda2a2
Bill Keuntje <bill.keuntje@gmail.com>
2021-06-30 15:53:08 -0400

    Add Prism for syntax highlighting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+// Warning: ambient access
 /* global window, document, fetch, setTimeout */
+/* global Element, HTMLElement */
 // @ts-check
 
 import htm from 'htm';
 import m from 'mithril';
 import { getEthProvider } from 'rchain-api';
+import Prism, { highlightElement } from 'prismjs';
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
 import { unwrap, buildUI, makeBusy, ckControl } from './participate';
+
+// WARNING: ambient access
+// Deal with "disappearing" newlines
+Prism.hooks.add('before-sanity-check', (env) => {
+  if (!(env.element instanceof HTMLElement)) return;
+  env.code = env.element.innerText;
+});
 
 // WARNING: ambient access
 document.addEventListener('DOMContentLoaded', () => {
@@ -23,5 +34,12 @@ document.addEventListener('DOMContentLoaded', () => {
     getEthProvider: () => getEthProvider({ window }),
     mount: (selector, control) => m.mount($(selector), control),
     hostname: document.location.hostname,
+    /**
+         * @param {string} language
+         * @param {import('prismjs').Grammar} grammar
+         */
+    setGrammar: (language, grammar) => {
+      Prism.languages[language] = grammar;
+    },
   });
 });
