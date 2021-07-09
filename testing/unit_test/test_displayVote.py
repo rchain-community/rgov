f5c9850eb9286791100cc02e7504ccee82d6d559
Tom Moulton <tom@rnode.moulton.us>
2021-07-09 06:23:16 -0400

    Use anonymous keys instead of PK.generate (ocap)

--- testing/unit_test/test_displayVote.py
+++ testing/unit_test/test_displayVote.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
-from rchain.crypto import PrivateKey
 from pyrgov.rgov import rgovAPI
 
 rgov = rgovAPI('localhost')
-new1 = PrivateKey.generate()
-new2 = PrivateKey.generate()
+new1 = rgov.get_private_key('anonymous')
+new2 = rgov.get_private_key('anonymous')
 admin = rgov.get_private_key('bootstrap')
 
 balance = rgov.checkBalance(admin.get_public_key().get_rev_address())
