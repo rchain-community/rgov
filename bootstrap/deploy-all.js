7f8b7a2b4ecb9ebb49b5dabb0eda0c699a007f91
david405 <davidasamonye@gmail.com>
2021-12-29 18:30:46 +0100

    fixed deploy-all

---
+++ bootstrap/deploy-all.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+const { join, resolve } = require('path');
+const { readdir } = require('fs').promises;
+
+const console = require('console');
+
+const shell = require('./cli-utils/exec_shell.js');
+const { deploy } = require('./cli-utils/deploy_script');
+
+console.log('Cloning into rchain. This may take a while');
+
+try {
+shell(`git clone https://github.com/rchain/rchain.git || (cd rchain && git pull)`)
+} catch (err) {
+    console.log('Failed')
+}
+
+const directory = join(__dirname, 'rchain');
+
+async function* getFiles(dir) {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    const res = resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* getFiles(res);
+    } else {
+      yield res;
+    }
+  }
+}
+function forAwait(asyncIter, f) {
+  asyncIter.next().then(({ done, value }) => {
+    if (done) return;
+    f(value);
+    forAwait(asyncIter, f);
+  });
+}
+const ALLNETWORKS = require('./networks');
+const network = 'localhost';
+const privatekey_f = path.join(__dirname, 'PrivateKeys/pk.bootstrap');
+
+
+forAwait(getFiles(directory), (x) => {
+  if (x.endsWith('.rho') && !x.includes('test') && !x.includes('examples')) {
+    deploy(console, ALLNETWORKS, x, privatekey_f, network);
+  }
+});
+
+// TODO: Get propose working!!!!
+
+// TODO: propose 
+// TODO: Create snapshot of rchain core 
+// TODO: Deploys dictionary.rho
+// TODO: Propose
+// TODO: Create master dictionary
+// TODO: Write Mater URI to MasterURI.localhost.json
+// TODO: Deploy rgov standard contract
+// TODO: Propose 
+// TODO: Create snapshot
+
+//run-rnode
+//restore-snapshot
--- bootstrap/deploy.js
+++ bootstrap/deploy.js
@@ -3,95 +3,25 @@
 const rchainToolkit = require('rchain-toolkit');
 const fs = require('fs');
 const path = require('path');
-const readline = require('readline');
-const { stdin: input, stdout: output } = require('process');
+
+const { deploy } = require('./cli-utils/deploy_script');
 
 const ALLNETWORKS = require('./networks');
 const console = require('console');
-const network_argument = 'localhost';
 
-const rl = readline.createInterface({ input, output });
+const argv = require('minimist')(process.argv.slice(2));
+let privatekey_f = argv.pk;
+let rholang_files = argv._;
+let network = argv.network;
+if (privatekey_f == undefined) {
+  privatekey_f = path.join(__dirname, 'PrivateKeys/pk.bootstrap');
+  console.log(`Private Key undefined, defaulting to ${privatekey_f}`);
+}
+if(network == undefined) {
+  network = 'localhost';
+  console.log(`Network undefined, defaulting to ${network}`);
+}
 
-let privateKeyDir;
-let privateKeyFile;
-let privateKey;
-
-// joining path of directory
-const directoryPath = path.join(__dirname, 'PrivateKeys');
-// passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-  // handling error
-  if (err) {
-    return console.log('Unable to scan directory: ' + err);
-  }
-  privateKeyDir = files;
-});
-
-// Get user input and use that to select a pk file
-rl.question(
-  `Select private key \n 1) alpha \n 2) bravo \n 3) charlie \n 
-Leave default to use bootstrap \n`,
-  (answer) => {
-    switch (answer) {
-      case '1':
-        privateKeyFile = privateKeyDir[0];
-        break;
-      case '2':
-        privateKeyFile = privateKeyDir[2];
-        break;
-      case '3':
-        privateKeyFile = privateKeyDir[3];
-        break;
-      default:
-        privateKeyFile = privateKeyDir[1];
-    }
-    rl.close();
-
-    console.log(privateKeyFile);
-
-    try {
-      privateKey = fs.readFileSync(`./PrivateKeys/${privateKeyFile}`, 'utf8');
-    } catch (err) {
-      console.error(err);
-    }
-
-    const publicKey = rchainToolkit.utils.publicKeyFromPrivateKey(privateKey);
-    
-    const rholang_files = process.argv.slice(2);
-
-    // run deploy function with rholang file argument
-    const deploy = async (rholang_f) => {
-      const rholang = fs.readFileSync(rholang_f, 'utf8');
-      const validAfterBlockNumber = await rchainToolkit.http.validAfterBlockNumber(
-        ALLNETWORKS[network_argument].observerBase,
-      );
-
-      const deployOptions = rchainToolkit.utils.getDeployOptions(
-        'secp256k1',
-        new Date().valueOf(),
-        rholang,
-        privateKey,
-        publicKey,
-        1,
-        100000,
-        validAfterBlockNumber,
-      );
-
-      let deployResponse;
-      try {
-        deployResponse = await rchainToolkit.http.deploy(
-          ALLNETWORKS[network_argument].observerBase,
-          deployOptions,
-        );
-      } catch (err) {
-        console.log(err);
-      }
-
-      console.log(deployResponse);
-    };
-
-    for (let i = 0; i < rholang_files.length; i += 1) {
-      deploy(rholang_files[i]);
-    }
-  },
-);
+for (let i = 0; i < rholang_files.length; i += 1) {
+  deploy(console, ALLNETWORKS, rholang_files[i], privatekey_f, network);
+}
