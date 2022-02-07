fcf304089e8f29c87d390678a16ed70aff3f3ee2
Kunj <bill.keuntje@gmail.com>
2022-02-07 11:09:51 -0500

    Add feature contracts to deploy

--- bootstrap/master-dictionary-setup/deploy-rgov-contracts.js
+++ bootstrap/master-dictionary-setup/deploy-rgov-contracts.js
@@ -16,8 +16,6 @@ const deployRgovContract = async () => {
     let directoryURI;
     let result;
 
-    const directory = join(__dirname, '../../rholang');
-
     // get rholang files from rchain directory function
     async function* getFiles(dir) {
         const dirents = await readdir(dir, { withFileTypes: true });
@@ -38,20 +36,32 @@ const deployRgovContract = async () => {
         });
     }
      // deploy rgov contracts
-     forAwait(getFiles(directory), (x) => {
-         async function deployContract() {
+    forAwait(getFiles(join(__dirname, '../../rholang/core')), (x) => {
+        async function deployContract() {
 
-        // exclude rgov contracts that should not be written into the master dictionary
-         if (x.endsWith('.rho') && !x.includes('CrowdFund') && !x.includes('memberIdGovRev') && !x.includes('RevIssuer') && !x.includes('MemberDirectory')) {
+            // exclude rgov contracts that should not be written into the master dictionary
+            if (x.endsWith('.rho') && !x.includes('CrowdFund') && !x.includes('memberIdGovRev') && !x.includes('RevIssuer')) {
 
-             directoryURI = await easyDeploy(console, ALLNETWORKS, x, privatekey_f, network);
+                directoryURI = await easyDeploy(console, ALLNETWORKS, x, privatekey_f, network);
 
-            await generateRholangContract(directoryURI[1], directoryURI[2]);
+                await generateRholangContract(directoryURI[1], directoryURI[2]);
 
-         }
-
+            }
         }
-        deployContract();
-     });
+        return deployContract();
+    }).then(_ =>
+    // deploy other contracts
+    forAwait(getFiles(join(__dirname, '../../rholang/feature')), (x) => {
+        async function deployContract() {
+
+            // exclude rgov contracts that should not be written into the master dictionary
+            if (x.endsWith('.rho')) {
+                directoryURI = await easyDeploy(console, ALLNETWORKS, x, privatekey_f, network);
+                await generateRholangContract(directoryURI[1], directoryURI[2]);
+
+            }
+        }
+        return deployContract();
+    }));
 }
 deployRgovContract().then(_ => propose().then(res => console.log(res)));
